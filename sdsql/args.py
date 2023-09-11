19e89b521c1a039ce18632d6efec2c89ee15f69d
nonames666 <57211749+nonames666@users.noreply.github.com>
2023-09-11 15:41:47 +0800

    Delete sdsql directory

--- sdsql/args.py
+++
@@ -1,4 +0,0 @@
-import torch
-torch.cuda.set_device(3)
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
--- sdsql/sqlova/args.py
+++
@@ -1,4 +0,0 @@
-import torch
-# torch.cuda.set_device(4)
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
